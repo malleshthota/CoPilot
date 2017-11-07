877857a4d54321d22fbe9297f4e703f54787c337
Mallesh Thota <mallesh.thota@outlook.com>
2017-11-08 00:40:04 +0530

    Logic change

--- ShippingPilot/Form1.cs
+++ ShippingPilot/Form1.cs
@@ -418,35 +418,38 @@ namespace ShippingPilot
 
         public void ExportToExcel()
         {
-            DataSet ds = new DataSet("PilotResponse");
-            ds.Tables.Add(table);
-
-            //Creae an Excel application instance
-            Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
-            Microsoft.Office.Interop.Excel.Workbook excelWorkBook = excelApp.Workbooks.Open(txtResponsePath.Text);
-
-            foreach (DataTable table in ds.Tables)
+            if (table.Rows.Count > 0)
             {
-                //Add a new worksheet to workbook with the Datatable name
-                Microsoft.Office.Interop.Excel.Worksheet excelWorkSheet = excelWorkBook.Sheets.Add();
-                excelWorkSheet.Name = $"Pilot Response_{DateTime.Now.ToString().Replace("-", "").Replace(":", "").Replace(" ", "")}";
+                DataSet ds = new DataSet("PilotResponse");
+                ds.Tables.Add(table);
 
-                for (int i = 1; i < table.Columns.Count + 1; i++)
-                {
-                    excelWorkSheet.Cells[1, i] = table.Columns[i - 1].ColumnName;
-                }
+                //Creae an Excel application instance
+                Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+                Microsoft.Office.Interop.Excel.Workbook excelWorkBook = excelApp.Workbooks.Open(txtResponsePath.Text);
 
-                for (int j = 0; j < table.Rows.Count; j++)
+                foreach (DataTable table in ds.Tables)
                 {
-                    for (int k = 0; k < table.Columns.Count; k++)
+                    //Add a new worksheet to workbook with the Datatable name
+                    Microsoft.Office.Interop.Excel.Worksheet excelWorkSheet = excelWorkBook.Sheets.Add();
+                    excelWorkSheet.Name = $"Pilot Response_{DateTime.Now.ToString().Replace("-", "").Replace(":", "").Replace(" ", "")}";
+
+                    for (int i = 1; i < table.Columns.Count + 1; i++)
                     {
-                        excelWorkSheet.Cells[j + 2, k + 1] = table.Rows[j].ItemArray[k].ToString();
+                        excelWorkSheet.Cells[1, i] = table.Columns[i - 1].ColumnName;
+                    }
+
+                    for (int j = 0; j < table.Rows.Count; j++)
+                    {
+                        for (int k = 0; k < table.Columns.Count; k++)
+                        {
+                            excelWorkSheet.Cells[j + 2, k + 1] = table.Rows[j].ItemArray[k].ToString();
+                        }
                     }
                 }
+                excelWorkBook.Save();
+                excelWorkBook.Close();
+                excelApp.Quit();
             }
-            excelWorkBook.Save();
-            excelWorkBook.Close();
-            excelApp.Quit();
         }
     }
 }
